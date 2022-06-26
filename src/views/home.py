5711d389901801cb14dd084ed8e5b7c53abbd5aa
Ajax Lima <ajaxlima94@gmail.com>
2022-06-26 13:40:26 -0300

    Feat: Test to show data printers

--- src/views/home.py
+++ src/views/home.py
@@ -1,4 +1,13 @@
 from flask import render_template
 
+printers = {
+        'identify':['123', '234', '567'],
+        'model':['Epson', 'Brother', 'Teste2'],
+        'sector': ['TI', 'Teste2', 'teste3'],
+        'date_purchased': ['2-2-2', '1-1-1', '6-6-6'],
+        'maintenances': [0, 5, 6]
+    }
+
 def homepage():
-    return render_template('home.html', selected='home')
+    length = len(printers['identify'])
+    return render_template('home.html', selected='home', printers=printers, length=length)
